6783ca4c0d2febcce206e68163264c199a391f3a
Cornelius Schumacher <schumacher@kde.org>
2018-11-06 10:52:25 +0100

    Use stdout argument
    Seems like capture_output is only in 3.7
    Signed-off-by: Cornelius Schumacher <schumacher@kde.org>

--- hello3/test_hello.py
+++ hello3/test_hello.py
@@ -10,5 +10,5 @@ from pathlib import Path
 class TestHello(unittest.TestCase):
     def test_hello(self):
         hello = (Path(os.path.dirname(__file__)) / "hello").resolve()
-        result = subprocess.run([hello], capture_output=True)
+        result = subprocess.run([hello], stdout=subprocess.PIPE)
         self.assertEqual(result.stdout.decode("utf-8"), "Hello, dear world!\nHello, people!\n")
